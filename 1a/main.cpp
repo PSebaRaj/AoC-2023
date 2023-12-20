1ba60913d62e706edc9ac4b251895d7f8a68f907
psebaraj <patrick.sebaraj@gmail.com>
2023-12-20 13:52:53 -0500

    day 1 complete

---
+++ 1a/main.cpp
@@ -0,0 +1,24 @@
+#include <cstdint>
+#include <fstream>
+#include <string>
+
+int find_calibration_value(std::string line) {
+	std::string first (1, line[line.find_first_of("123456789")]);
+	std::string last (1, line[line.find_last_of("0123456789")]);
+
+	std::size_t value = std::stoi(first + last);
+	return value;
+}
+
+int main() {
+	std::ifstream file("input.txt");
+	std::int32_t sum = 0;
+	if (file.is_open()) {
+		std::string line;
+		while (std::getline(file, line)) {
+			sum += find_calibration_value(line);
+		}
+		file.close();
+	}
+	printf("Result: %d\n", sum);
+}
